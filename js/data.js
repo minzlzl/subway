52c7265d0d5fa6ac6f23d578d774b9e5f24a69c6
minzlzl <133857306+minzlzl@users.noreply.github.com>
2023-10-26 12:53:08 +0900

    231026

--- js/data.js
+++ js/data.js
@@ -1,20 +1,38 @@
-const con2Ul = document.querySelector('.con2_wrap>ul'),
-      con2H2 = document.querySelector('.con2_wrap h2');
-      
-
+const con2Ulsandwich = document.querySelector('.sandwich .con2_wrap>ul'),
+      con2H2sandwich = document.querySelector('.sandwich .con2_wrap h2');
 fetch("js/sandwich.json")
 .then(res=>res.json())
 .then(json=>{
   json.sandwich.forEach((v,k)=>{
-    con2Ul.innerHTML += `
-                        <li>
-                          <div class="con2_box">
-                            <figure><img src="${json.sandwich[k].img}" alt=""/></figure>
-                            <p>${json.sandwich[k].name}</p>
-                          </div>
-                          <span>자세히 보기 ></span>
-                        </li>
-                        `
-    con2H2.innerHTML = `전체상품(${json.sandwich.length}개)`
+    con2Ulsandwich.innerHTML += `
+                                <li>
+                                  <div class="con2_box">
+                                    <figure><img src="${json.sandwich[k].img}" alt=""/></figure>
+                                    <p>${json.sandwich[k].name}</p>
+                                  </div>
+                                  <span>자세히 보기 ></span>
+                                </li>
+                                `
+    con2H2sandwich.innerHTML = `전체상품(${json.sandwich.length}개)`
     });
+})
+
+
+const con2Ulunit = document.querySelector('.unit .con2_wrap>ul'),
+      con2H2unit = document.querySelector('.unit .con2_wrap h2');
+fetch("js/unit.json")
+.then(res=>res.json())
+.then(json=>{
+  json.unit.forEach(function(v2,k2){
+    con2Ulunit.innerHTML += `
+                            <li>
+                              <div class="con2_box">
+                                <figure><img src="${json.unit[k2].img}" alt=""/></figure>
+                                <p>${json.unit[k2].name}</p>
+                              </div>
+                              <span>자세히 보기 ></span>
+                            </li>
+                            `
+  con2H2unit.innerHTML = `전체상품(${json.unit.length}개)`
+  })
 })
